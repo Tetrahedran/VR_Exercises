f6b5bec7372c909c0292d163711b90d9b2ec6a70
Patrick <paddy38@web.de>
2021-06-28 17:37:52 +0200

    added first sample scene

---
+++ Assets/MoveWith.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWith : MonoBehaviour
+{
+    public Vector3 offset;
+
+    public bool invert;
+
+    private Vector3 startPos;
+
+    private Vector3 targetPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+        targetPos = startPos + offset;
+    }
+
+    public void move(bool toStartPos, float position)
+    {
+        if(toStartPos ^ invert)
+        {
+            moveTransform(targetPos, startPos, position);
+        }
+        else
+        {
+            moveTransform(startPos, targetPos, position);
+        }
+    }
+
+    private void moveTransform(Vector3 Start, Vector3 End, float Position)
+    {
+        transform.position = Vector3.Slerp(Start, End, Position);
+    }
+}
---
+++ Assets/Platte.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Platte : MonoBehaviour
+{
+    public float speed = 1;
+
+    public MoveWith[] attached;
+
+    Coroutine moveC = null;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject other = collision.gameObject;
+        if(other.tag == "Placable")
+        {
+            movePlatte(false);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        GameObject other = collision.gameObject;
+        if (other.tag == "Placable")
+        {
+            movePlatte(true);
+        }
+    }
+
+    private void movePlatte(bool up)
+    {
+        Debug.Log("moving " + (up ? "up" : "down"));
+        if (moveC == null)
+        {
+            moveC = StartCoroutine(move(up));
+        }
+    }
+
+    private IEnumerator move(bool up)
+    {
+        float increase = 0.005f;
+        float waitingTime = increase / speed;
+        yield return new WaitForSeconds(0.1f);
+        for(float i = 0; i < 1; i += increase)
+        {
+            foreach(MoveWith other in attached)
+            {
+                other.move(up, i);
+            }
+            yield return new WaitForSecondsRealtime(waitingTime);
+        }
+        moveC = null;
+    }
+}
